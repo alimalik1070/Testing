ddb63d590df554e3260028d9151424c39ebce653
alimalik1070 <l215477@lhr.nu.edu.pk>
2024-12-03 22:51:58 +0500

    Update LoginApp.java

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -57,6 +57,7 @@ public class LoginApp extends JFrame {
     }
 
     public String authenticateUser(String email) {
+        System.out.println("Testing login process...");
         String userName = null;
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             String query = "SELECT name FROM User WHERE Email = ?";
