df3e7785320df56d112aa39942cfee047c9b0753
alimalik1070 <l215477@lhr.nu.edu.pk>
2024-12-04 23:28:52 +0500

    Update LoginApp.java

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -57,24 +57,22 @@ public class LoginApp extends JFrame {
     }
 
     public String authenticateUser(String email) {
-        System.out.println("Testing login process...");
-        String userName = null;
-        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            String query = "SELECT name FROM User WHERE Email = ?";
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, email);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                userName = rs.getString("Name");
-            }
-            rs.close();
-            stmt.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+    String userName = null;
+    try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+        String query = "SELECT Name, Password FROM User WHERE Email = ? AND Password = ?";
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, email);
+        stmt.setString(2, password);
+        ResultSet rs = stmt.executeQuery();
+        
+        if (rs.next()) {
+            userName = rs.getString("Name");
         }
-        return userName;
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+    return userName;
+}
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
